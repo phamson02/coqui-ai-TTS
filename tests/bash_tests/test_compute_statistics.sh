3321b0d17f223b4ec1f54bcf90cbba7884d15e60
Enno Hermann <enno.hermann@idiap.ch>
2024-12-13 11:24:31 +0100

    test: consistently name test config.json files

--- tests/bash_tests/test_compute_statistics.sh
+++ tests/bash_tests/test_compute_statistics.sh
@@ -3,4 +3,4 @@ set -xe
 BASEDIR=$(dirname "$0")
 echo "$BASEDIR"
 # run training
-CUDA_VISIBLE_DEVICES="" python TTS/bin/compute_statistics.py --config_path $BASEDIR/../inputs/test_glow_tts.json --out_path $BASEDIR/../outputs/scale_stats.npy
+CUDA_VISIBLE_DEVICES="" python TTS/bin/compute_statistics.py --config_path $BASEDIR/../inputs/test_glow_tts_config.json --out_path $BASEDIR/../outputs/scale_stats.npy
